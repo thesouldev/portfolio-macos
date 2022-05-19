ba0c9116d54cfa748da74953b5fd9dfe3923ee92
Sasidharan V <52165229+thesouleddev@users.noreply.github.com>
2022-05-20 00:34:52 +0530

    Initial Commit

--- Portfolio/ContentView.swift
+++ Portfolio/ContentView.swift
@@ -7,15 +7,51 @@
 
 import SwiftUI
 
+struct Option: Hashable {
+    let title: String
+    let imageName: String
+}
+
 struct ContentView: View {
+    let options : [Option] = [
+        .init(title: "About", imageName: "info.circle"),
+        .init(title: "Experience", imageName: "cloud"),
+        .init(title: "Contact", imageName: "pencil.circle"),
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ListView(options: options).frame(minWidth: 300)
+            MainView()
+        }
+    }
+}
+
+struct ListView: View {
+    let options : [Option]
+    var body: some View {
+        VStack {
+            ForEach(options, id: \.self) { option in
+                HStack {
+                    Image(systemName: option.imageName)
+                    Text(option.title)
+                    
+                }
+                .padding(3)
+            }
+        }
+    }
+}
+
+struct MainView: View {
+    var body: some View {
+        Text("Main 1")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
