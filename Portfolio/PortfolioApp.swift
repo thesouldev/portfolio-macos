d1dce6f6ad3bf545a1b16671aadb94e46bec46e8
Sasidharan V <52165229+thesouleddev@users.noreply.github.com>
2022-05-19 23:51:48 +0530

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Sasidharan Velu on 19/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
